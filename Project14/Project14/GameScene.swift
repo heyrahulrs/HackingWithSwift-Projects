09051ced067c9cb9e69084516b34f0c043f2eda7
Rahul <rahulrs0029@gmail.com>
2019-05-19 11:52:30 +0530

    Added Project14. - commit #1

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Rahul Sharma on 5/19/19.
+//  Copyright © 2019 Rahul Sharma. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var scoreLabel: SKLabelNode!
+    
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.position = CGPoint(x: 8, y: 8)
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.fontSize = 48
+        addChild(scoreLabel)
+        
+    }
+    
+}
